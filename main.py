bfea9066b1d353f6b47cee321fb40fb8fb147fcf
juneshone <shamrova.julia@mail.ru>
2024-02-02 19:23:40 +0300

    Для сложения строк использовала f-строки

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def shorten_link(token, url):
 
 def count_clicks(token, link):
     link_parse = urlparse(link)
-    link = link_parse.netloc + link_parse.path
+    link = f'{link_parse.netloc}{link_parse.path}'
     url_bitly1 = f'https://api-ssl.bitly.com/v4/bitlinks/{link}/clicks/summary'
     response = requests.get(url_bitly1, headers={'Authorization': token})
     response.raise_for_status()
@@ -24,7 +24,7 @@ def count_clicks(token, link):
 
 def is_bitlink(token, url):
     link_parse = urlparse(url)
-    link = link_parse.netloc + link_parse.path
+    link = f'{link_parse.netloc}{link_parse.path}'
     url_bitly2 = f'https://api-ssl.bitly.com/v4/bitlinks/{link}'
     response = requests.get(url_bitly2, headers={'Authorization': token})
     return response.ok
