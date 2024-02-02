66b1320f485f9d2f461993a838079e0e84a316c6
juneshone <shamrova.julia@mail.ru>
2024-02-02 19:53:17 +0300

    Отформатировала код

--- main.py
+++ main.py
@@ -4,30 +4,35 @@ from dotenv import load_dotenv
 import os
 
 
-
 def shorten_link(token, url):
     bitly_url = 'https://api-ssl.bitly.com/v4/bitlinks'
     data = {"long_url": url}
-    response = requests.post(bitly_url, json=data,  headers={'Authorization': token})
+    headers = {'Authorization': token}
+    response = requests.post(bitly_url, json=data,  headers=headers)
     response.raise_for_status()
     bitlink = response.json().get('link')
     return bitlink
 
+
 def count_clicks(token, link):
     parsed = urlparse(link)
     link = f'{parsed.netloc}{parsed.path}'
+    headers = {'Authorization': token}
     bitly_url = f'https://api-ssl.bitly.com/v4/bitlinks/{link}/clicks/summary'
-    response = requests.get(bitly_url, headers={'Authorization': token})
+    response = requests.get(bitly_url, headers=headers)
     response.raise_for_status()
     clicks_count = response.json().get('total_clicks')
     return clicks_count
 
+
 def is_bitlink(token, url):
     parsed = urlparse(url)
     link = f'{parsed.netloc}{parsed.path}'
+    headers = {'Authorization': token}
     bitly_url = f'https://api-ssl.bitly.com/v4/bitlinks/{link}'
-    response = requests.get(bitly_url, headers={'Authorization': token})
+    response = requests.get(bitly_url, headers=headers)
     return response.ok
+
 
 def main():
     load_dotenv()
@@ -35,19 +40,17 @@ def main():
     user_input = input('Введите ссылку: ')
     if is_bitlink(token, user_input):
         try:
-            clicks_count = count_clicks(token, user_input)
+            count_clicks(token, user_input)
         except requests.exceptions.HTTPError:
-            print('Вы ввели неверную ссылку')  # если ошибка
-        print('Количество кликов по ссылке за все дни', count_clicks(token, user_input))  # если все впорядке
+            print('Вы ввели неверную ссылку')
+        print('Количество кликов по ссылке за все дни', count_clicks(token, user_input))
     else:
         try:
-            bitlink = shorten_link(token, user_input)  # пишется код, который потенциально может вызвать ошибку
+            shorten_link(token, user_input)
         except requests.exceptions.HTTPError:
             print('Вы ввели неверную ссылку!')  # если ошибка
         print('Битлинк', shorten_link(token, user_input))  # если все впорядке
 
+
 if __name__ == '__main__':
     main()
-
-# https://bit.ly/42reuxz
-
